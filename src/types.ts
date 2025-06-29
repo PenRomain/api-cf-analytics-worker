06abd460996b2e5197c62d95077ca2e926cd7e58
PenRomain <romandreevich@icloud.com>
2025-06-29 17:03:55 +0500

    added kv

--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,5 @@ import type { D1Database, KVNamespace } from "@cloudflare/workers-types";
 export interface Env {
   DB: D1Database;
   ASSET_NAMESPACE: KVNamespace;
+  "__cf-analytics-worker-workers_sites_assets": KVNamespace;
 }
